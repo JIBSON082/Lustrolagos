99e4b26e5a98b9be21b9c239d22766e296b917a9
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-04-29 21:37:47 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,11 @@
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    formats: ["image/webp", "image/avif"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    minimumCacheTTL: 60,
+  },
+};
+
+export default nextConfig;
