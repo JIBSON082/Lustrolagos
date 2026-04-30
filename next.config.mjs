71aadb6f05aa6a694a5f8cdcb0d901e91db0e3bc
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-04-30 01:16:23 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
